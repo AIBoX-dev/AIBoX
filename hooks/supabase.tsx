9ebceb77ff57dba005641eb6e3a28d398f52c61e
Ftps <63702646+Tps-F@users.noreply.github.com>
2023-05-12 19:58:43 +0900

    login with google

---
+++ hooks/supabase.tsx
@@ -0,0 +1,40 @@
+import { useRouter } from 'next/router';
+import { createClient } from '@supabase/supabase-js'
+
+const supabase_url = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL as string
+const supabase_key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+
+console.log(supabase_url)
+console.log(supabase_key)
+const supabase = createClient(supabase_url, supabase_key)
+
+export const useAuth = () => {
+    const router = useRouter();
+    const signInWithGoogle = async () => {
+        try {
+            const { data, error } = await supabase.auth.signInWithOAuth({
+                provider: 'google',
+                options: {
+                    queryParams: {
+                        access_type: 'offline',
+                        prompt: 'consent',
+                    },
+                },
+            });
+            // ユーザーが存在するかを確認して、いなかったら表示名設定とかのページに行きたい Ftps
+            await router.push("/");
+            if (error) {
+                throw error;
+            }
+            
+            await router.push("/");
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    return {
+        signInWithGoogle
+    }
+}
+
+    
