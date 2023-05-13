00dae2bcefc6d1335f7486c270927ef532bbdcf2
Ftps <ftpsflandre@gmail.com>
2023-05-13 21:07:44 +0900

    rewrite and password_check

--- hooks/check.tsx
+++ hooks/check.tsx
@@ -1,17 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 import { useTranslation } from "next-i18next";
 
 export function Checks() {
     const { t } = useTranslation("common");
-    const [email, setEmail] = useState('')
-    
+    const [data, setData] = useState({
+        // value
+        email: "",
+        password: "",
+        confirm_password: "",
+        // status
+        email_status: "",
+        password_status: "",
+        confirm_status: "",
+    });
+
     const regex_email = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    let email_state;
-    
-    if (email && !email.match(regex_email)) {
-        email_state = t("Check.wrongemail")
-    }
+
+    const CheckEmail = () => {
+        if (data.email && !data.email.match(regex_email)) {
+            setData({ ...data, email_status: t("Check.wrongemail") });
+            console.log(data.email);
+            console.log(data.email_status);
+        } else {
+            setData({ ...data, email_status: "" });
+            console.log(data.email);
+        }
+    };
+    const CheckPassword = () => {
+        // パスワードの企画をまた考える
+    };
+
+    useEffect(() => {
+        CheckEmail();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [data.email]);
+
+    useEffect(() => {
+        CheckPassword();
+    }, [data.password]);
+
     return {
-        email, setEmail, email_state
+        data,
+        setData,
     };
 }
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -32,7 +32,7 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => ({
 export default function Signup() {
     const { t } = useTranslation("common");
     const { signInWithGoogle } = useAuth();
-    const { email, setEmail, email_state} = Checks();
+    const { data, setData } = Checks();
     return (
         <>
             <Header />
@@ -63,10 +63,10 @@ export default function Signup() {
                             placeholder={t("Signup.email")}
                             contentLeft={<Mail />}
                             aria-labelledby="email"
-                            value={email}
-                            onChange={(event) => setEmail(event.target.value)}
+                            value={data.email}
+                            onChange={(event) => setData({...data, email: event.target.value})}
                         />
-                        <p>{email_state}</p>
+                        <p>{data.email_status}</p>
                         <Spacer y={1} />
                         <Input
                             clearable
@@ -79,6 +79,8 @@ export default function Signup() {
                             css={{ mb: '6px' }}
                             aria-labelledby="password"
                             type="password"
+                            value = {data.password}
+                            onChange={(event) => setData({...data, password: event.target.value})}
                         />
                         <Spacer y={1} />
                         <Input
