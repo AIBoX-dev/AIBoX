a8f53a94c83b2cf9b0cb6f8d0c6486bd82367357
kokohachi <99192596+Kokohachi@users.noreply.github.com>
2023-05-18 21:03:15 +0900

    create setup backend

--- hooks/supabase.tsx
+++ hooks/supabase.tsx
@@ -5,25 +5,25 @@ import { useRouter } from "next/router";
 const supabase_url = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL as string;
 const supabase_key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-import {Checks} from "@/hooks/check"
+import { Checks } from "@/hooks/check"
 const crypto = require('crypto');
 const supabase = createClient(supabase_url, supabase_key);
 
-const insertUser = async (email: string, password: string, userdata: { [key: string]: any} ) => {
+const insertUser = async (email: string, password: string, userdata: { [key: string]: any }) => {
     const salt = crypto.randomBytes(16).toString('hex');
     const hash = crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512').toString('hex')
     const { data, error } = await supabase
-    .from('user')
-    .insert([
-        {
-            user_id: userdata["user"]["id"],
-            email: email,
-            password_hash: hash,
-            salt: salt,
-            created_at: userdata["user"]["created_at"],
-            last_login: userdata["user"]["identities"]["last_sign_in_at"],
-            updated_at: userdata["user"]["updated_at"],
-        },
+        .from('user')
+        .insert([
+            {
+                user_id: userdata["user"]["id"],
+                email: email,
+                password_hash: hash,
+                salt: salt,
+                created_at: userdata["user"]["created_at"],
+                last_login: userdata["user"]["identities"]["last_sign_in_at"],
+                updated_at: userdata["user"]["updated_at"],
+            },
         ]);
 
     if (error) {
@@ -32,6 +32,31 @@ const insertUser = async (email: string, password: string, userdata: { [key: str
         //errorコードの処理を
     }
 };
+
+const updateDob = async (id: number, dob: string) => {
+    const { data, error } = await supabase
+        .from('user')
+        .update({ dob: dob })
+        .eq('id', id)
+};
+
+
+const insertProfile = async (id: number, user_id: string, display_name: string, description: string, created_at: string, updated_at: string) => {
+    const { data, error } = await supabase
+        .from('profile')
+        .insert([
+            {
+                id: id,
+                user_id: user_id,
+                display_name: display_name,
+                description: description,
+                created_at: created_at,
+                updated_at: updated_at,
+            },
+        ]);
+};
+
+
 
 export const useAuth = () => {
     const router = useRouter();
@@ -56,7 +81,7 @@ export const useAuth = () => {
         }
     };
 
-    const createUser = async ( email: string, password: string): Promise<void> => {
+    const createUser = async (email: string, password: string): Promise<void> => {
         try {
             const { data, error } = await supabase.auth.signUp({
                 email: email,
@@ -70,17 +95,31 @@ export const useAuth = () => {
 
             console.error(error);
         }
-    }
+    };
 
-    const loginWithPassword = async ( email: string, password: string): Promise<void> => {
+
+    const createProfile = async (id: number, user_id: string, display_name: string, dob:string) => {
+        try {
+            const description = "Hello!"
+            const created_at = new Date().toISOString()
+            const updated_at = new Date().toISOString()
+            await insertProfile(id, user_id, display_name, description, created_at, updated_at)
+            await updateDob(id, dob)
+        } catch (error) {
+            console.error(error)
+        }
+    };
+
+
+    const loginWithPassword = async (email: string, password: string): Promise<void> => {
         try {
             console.log(email)
-            const { data, error }= await supabase.auth.signInWithPassword({
+            const { data, error } = await supabase.auth.signInWithPassword({
                 email: email,
                 password: password
             })
-            console.log({data, error})
-        } catch(error) {
+            console.log({ data, error })
+        } catch (error) {
             console.error(error)
         }
     }
@@ -92,7 +131,7 @@ export const useAuth = () => {
                 email: email,
             });
             console.log({ data, error });
-            }
+        }
         catch (error) {
             console.error(error);
         }
@@ -103,6 +142,8 @@ export const useAuth = () => {
         signInWithGoogle,
         createUser,
         loginWithPassword,
-        resendVerificationEmail
+        resendVerificationEmail,
+        createProfile
     };
 }
+
--- pages/setup.tsx
+++ pages/setup.tsx
@@ -1,10 +1,13 @@
 import { Pagination,Card, Text, Container, Input, Spacer, Button, Checkbox, Row, Image } from "@nextui-org/react";
 import { GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import React from 'react';
+
 import { AtSign, Eye, Calendar } from 'react-feather';
 import Header from '@/components/Header';
+import { useAuth } from '@/hooks/supabase';
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => ({
     props: {
@@ -17,6 +20,24 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => ({
 
 export default function Setup() {
     const { t } = useTranslation("common");
+    const router = useRouter();
+    const { id: uid } = router.query;
+    const { createProfile } = useAuth();
+    const [displayName, setDisplayName] = React.useState("");
+    const [dob, setDob] = React.useState("");
+    const [userId, setUserId] = React.useState("");
+
+    const handleProfile = () => {
+        try {
+            createProfile(uid as unknown as number, displayName, dob, userId);
+        }
+        catch (error) {
+            console.log(error);
+    };
+};
+
+
+
     return(
         <>
         <Header />
@@ -48,6 +69,7 @@ export default function Setup() {
                             placeholder={t("Setup.user_id")}
                             aria-labelledby="user id"
                             type="text"
+                            onChange={(e) => setUserId(e.target.value)}
                         />
                         <Spacer y={1} />
                         <Input
@@ -62,6 +84,7 @@ export default function Setup() {
                             css={{ mb: "6px" }}
                             aria-labelledby="display_name"
                             type="text"
+                            onChange={(e) => setDisplayName(e.target.value)}
                         />
                         <Spacer y={1} />
                         <Input
@@ -76,6 +99,7 @@ export default function Setup() {
                             aria-labelledby="display_name"
                             labelLeft={<Calendar />}
                             type="datetime-local"
+                            onChange={(e) => setDob(e.target.value)}
                         />
                         <Spacer y={1} />
                         <Button bordered color="gradient" auto>
