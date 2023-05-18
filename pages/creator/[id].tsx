488e55b0690792ec8166892b8da96b70680584bf
kokohachi <99192596+Kokohachi@users.noreply.github.com>
2023-05-18 21:03:13 +0900

    create setup backend

---
+++ pages/creator/[id].tsx
@@ -0,0 +1,29 @@
+
+import { Text, Container, Row, Col, Card, Spacer, Button, Input, Grid, Checkbox } from "@nextui-org/react";
+
+import { GetStaticProps } from "next";
+import { useRouter } from 'next/router'
+
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+
+
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+    props: {
+        ...(await serverSideTranslations(locale!, ["common"])),
+    },
+});
+
+export default function Creator() {
+    const { t } = useTranslation("common");
+    const router = useRouter()
+    const { id } = router.query
+    
+
+
+
+
+
