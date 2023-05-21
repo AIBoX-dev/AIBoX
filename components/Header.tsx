ad71e7f89b6704932f2abe61bf03bb4929a5b41f
kokohachi <99192596+Kokohachi@users.noreply.github.com>
2023-05-21 23:30:02 +0900

    atripe & header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,18 @@
-import { Navbar, Button, Link, Text } from "@nextui-org/react";
+import { Navbar, Button, Link, Text, User, Dropdown } from "@nextui-org/react";
 import { useRouter } from "next/router";
+
 import { useTranslation } from "next-i18next";
+
+import React from "react";
 import { AcmeLogo } from "./header/AcmeLogo";
 import { Layout } from "./header/Layout";
 
-interface Props {}
+interface Props { }
 
 export default function Header(props: Props) {
     const router = useRouter();
     const { t } = useTranslation("common");
+    const [logged, setLogged] = React.useState(true);
     return (
         <Layout>
             <Navbar isBordered variant="floating">
@@ -70,22 +74,55 @@ export default function Header(props: Props) {
                         </Link>
                     </Navbar.CollapseItem>
                 </Navbar.Collapse>
-                <Navbar.Content>
-                    <Navbar.Link color="primary" href="/login">
-                        {t("Header.login")}
-                    </Navbar.Link>
-                    <Navbar.Item>
-                        <Button
-                            auto
-                            color="error"
-                            flat
-                            as={Link}
-                            href="/signup"
-                        >
-                            {t("Header.signup")}
-                        </Button>
-                    </Navbar.Item>
-                </Navbar.Content>
+                {logged ? (
+                    <Dropdown placement="bottom-left">
+                        <Dropdown.Trigger>
+                            <User
+                                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                                name="Ariana Wattson"
+                            >
+                                <User.Link href="https://nextui.org/">@watsonari</User.Link>
+                            </User>
+                        </Dropdown.Trigger>
+                        <Dropdown.Menu color="secondary" aria-label="Avatar Actions">
+                            <Dropdown.Item key="profile" css={{ height: "$18" }} >
+                                <Text b color="inherit" css={{ d: "flex" }}>
+                                    Signed in as
+                                </Text>
+                                <Text b color="inherit" css={{ d: "flex" }}>
+                                    zoey@example.com
+                                </Text>
+                            </Dropdown.Item>
+                            <Dropdown.Item key="configurations" withDivider>Dashboard</Dropdown.Item>
+                            <Dropdown.Item key="settings" >
+                                Settings
+                            </Dropdown.Item>
+                            <Dropdown.Item key="help_and_feedback" withDivider>
+                                Help & Feedback
+                            </Dropdown.Item>
+                            <Dropdown.Item key="logout" color="error" withDivider>
+                                Log Out
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                ) : (
+                    <Navbar.Content>
+                        <Navbar.Link color="primary" href="/login">
+                            {t("Header.logiSn")}
+                        </Navbar.Link>
+                        <Navbar.Item>
+                            <Button
+                                auto
+                                color="error"
+                                flat
+                                as={Link}
+                                href="/signup"
+                            >
+                                {t("Header.signup")}
+                            </Button>
+                        </Navbar.Item>
+                    </Navbar.Content>
+                )}
             </Navbar>
         </Layout>
     );
--- hooks/stripe.tsx
+++ hooks/stripe.tsx
@@ -42,15 +42,59 @@ const getCustomer = async (id: string) => {
     return customer;
 }
 
-const createPlan = async (name: string, plan_id: string, price: number) => {
+const createPlan = async (name: string, plan_id: string, price: number, creator_id: string) => {
     const plan = await stripe.plans.create({
         amount: price,
-        currency: "usd",
+        currency: "jpy",
         interval: "month",
         product: {
             name: name,
         },
+        metadata: {
+            plan_id: plan_id,
+            creator_id: creator_id,
+        },
         id: plan_id,
     });
     return plan;
+}
+
+const getPlan = async (id: string) => {
+    const plan = await stripe.plans.retrieve(id);
+    return plan;
+}
+
+const createSubscription = async (customer_id: string, plan_id: string) => {
+    const subscription = await stripe.subscriptions.create({
+        customer: customer_id,
+        items: [
+            { plan: plan_id },
+        ],
+    });
+    return subscription;
+}
+
+const getSubscription = async (id: string) => {
+    const subscription = await stripe.subscriptions.retrieve(id);
+    return subscription;
+}
+
+const cancelSubscription = async (id: string) => {
+    const subscription = await stripe.subscriptions.del(id);
+    return subscription;
+}
+
+const createSubscriptionCheckoutSession = async (customer_id: string, plan_id: string, success_url: string, cancel_url: string) => {
+    const session = await stripe.checkout.sessions.create({
+        customer: customer_id,
+        line_items: [
+            {
+                price: plan_id,
+                quantity: 1,
+            },
+        ],
+        success_url: success_url,
+        cancel_url: cancel_url,
+    });
+    return session;
 }
