e5f4658155c879733bcd42851d1fb9f701429ed9
Ftps <ftpsflandre@gmail.com>
2023-06-03 21:28:36 +0900

    use directCreatorUpload

--- hooks/cfImages.ts
+++ hooks/cfImages.ts
@@ -1,36 +1,49 @@
-import axios from "axios";
+import axios from 'axios';
 
-const cloudflare_account_id = process.env
-    .NEXT_PUBLIC_CLOUDFLARE_ACCOUNT_ID as string;
+const cloudflare_account_id = process.env.NEXT_PUBLIC_CLOUDFLARE_ACCOUNT_ID as string;
 const cloudflare_token = process.env.NEXT_PUBLIC_CLOUDFLARE_TOKEN as string;
 
-export const images = () => {
-    const uploadImage = async (image: Blob) => {
-        try {
-            const form = new FormData();
-            form.append("requireSignedURLs", "true");
-            form.append("metadata", JSON.stringify({ key: "value" }));
-            form.append("file", image);
-
-            const { data } = await axios(
-                `https://api.cloudflare.com/client/v4/accounts/${cloudflare_account_id}/images/v1`,
-                {
-                    method: "POST",
-                    headers: {
-                        Authorization: `Bearer ${cloudflare_token}`,
-                        "Content-Type": "multipart/form-data",
-                    },
-                    data: form,
-                }
-            );
-
-            console.log(data);
-        } catch (e) {
-            console.log(e);
+export const cfImage = () => {
+  const getDirectCreatorUploadLink = async () => {
+    try {
+      const response = await axios.post(
+        `https://api.cloudflare.com/client/v4/accounts/${cloudflare_account_id}/images/v2/direct_upload`,
+        null,
+        {
+          headers: {
+            Authorization: `Bearer ${cloudflare_token}`,
+            'Content-Type': 'multipart/form-data',
+          },
+          params: {
+            requireSignedURLs: true,
+          },
+          data: {
+            metadata: JSON.stringify({ key: 'value' }),
+          },
         }
-    };
+        );
 
-    return {
-        uploadImage,
-    };
+      console.log(response.data);
+      return response.data.result.uploadURL
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const uploadImage = async (blob: Blob) => {
+    await getDirectCreatorUploadLink().then(async uploadURL => {
+      const formData = new FormData()
+      formData.append('file', blob)
+      const cloudflareResponse = await fetch(uploadURL, {
+        method: 'POST',
+        body: formData
+      })
+      console.log(await cloudflareResponse.json())
+    })
+  }
+
+
+  return {
+    uploadImage
+  }
 };
--- hooks/imageHandler.ts
+++ hooks/imageHandler.ts
@@ -1,15 +1,16 @@
-import { images } from "@/hooks/cfImages";
+import { cfImage } from "@/hooks/cfImages";
 import { Convert } from "@/hooks/convertImage";
 
 export const imageHandler = () => {
-    const { uploadImage } = images();
-    const { blobToFile, convertToWebP, saveBlob, getImageType } = Convert();
+    const { uploadImage } = cfImage();
+    const { blobToFile, convertToWebP, saveBlob} = Convert();
+
     const uploadProfileAvatar = async (blob: Blob, uid: string) => {
         const image = blobToFile(blob, uid);
-        convertToWebP(image).then((webpBlob) => {
-            console.log(webpBlob.type);
+        convertToWebP(image).then(async webpBlob => {
+            console.log(webpBlob.type)
+            await uploadImage(webpBlob)
         });
-        // await uploadImage(webp_image)
     };
 
     return {
